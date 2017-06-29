22bfbcc29a89b02be90ff32c8516f5749175981c
Arnab Sinha <ca32870@delta.org>
2017-06-29 11:39:07 -0700

    updated mot url

--- constants.js
+++ constants.js
@@ -41,7 +41,7 @@ module.exports = {
     "EE_PORT": process.env.PORT || 80,
     "EE_PORT_SSL": 443,
     "SEARCH_SERVICE_PORT": 443,
-    "SEARCH_SERVICE_HOST": "https://mot-heroku.deltadentalins.com/",
+    "SEARCH_SERVICE_HOST": "https://mot-heroku.deltadentalins.com",
     "SEARCH_SERVICE_PATH": "/api/cx/providerSearch/v1/providers"
   },
   "pit": {
